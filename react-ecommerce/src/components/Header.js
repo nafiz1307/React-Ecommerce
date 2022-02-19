991447dac7dca9aaeb892265756305bb6a49c2b6
Nafiz Zaman <55802844+nafiz1307@users.noreply.github.com>
2022-02-19 20:11:21 +0600

    header component complete

---
+++ react-ecommerce/src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Header.css";
+import logo from "../assets/logo.png";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <img className="header_logo" src={logo} alt="logo" />
+      <div className="header_search">
+        <input type="text" className="header_searchIn" />
+        <i class="material-icons header_search_icon">search</i>
+      </div>
+      <div className="header_nav">
+        <div className="header_option">
+          <span className="header_optionL1">Hello User</span>
+          <span className="header_optionL2">Sign In</span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionL1">Returns</span>
+          <span className="header_optionL2">& Orders</span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionL1">Your</span>
+          <span className="header_optionL2">Prime</span>
+        </div>
+      </div>
+    </div>
+  );
+};
