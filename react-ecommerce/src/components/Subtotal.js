05d344d9128cf5f0b1b913a886b773e5a07dcd0c
Nafiz Zaman <55802844+nafiz1307@users.noreply.github.com>
2022-02-26 09:31:05 +0600

    implementation of subtotal component

--- react-ecommerce/src/components/Subtotal.js
+++ react-ecommerce/src/components/Subtotal.js
@@ -14,8 +14,8 @@ const Subtotal = () => {
               <strong>{` ${value}`}</strong>
             </p>
             <small className="subtotal_gift">
-              <input type="checkbox"/>
-              This order contains a gift
+              <input type="checkbox" />
+              <span>This order contains a gift</span>
             </small>
           </>
         )}
@@ -26,6 +26,10 @@ const Subtotal = () => {
         thousandSeparator={true}
         prefix={"$"}
       />
+      <button className="btn waves-effect waves-light proceed">
+        Proceed to Checkout
+        <i class="material-icons right">send</i>
+      </button>
     </div>
   );
 }
